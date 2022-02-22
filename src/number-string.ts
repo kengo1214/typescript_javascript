37ec1362e37d0d2720ffb824208e3808d2a6d39b
Kobayashi Kengo <kken91914@gmail.com>
2022-02-22 21:53:48 +0900

    number型とstring型を追加

---
+++ src/number-string.ts
@@ -0,0 +1,14 @@
+export {};
+
+let year: number = 1999;
+// year = false;
+
+console.log(year);
+
+let age: number = 22;
+
+console.log(age);
+
+let name: string = "kengo";
+// name = 12;
+console.log(name);
