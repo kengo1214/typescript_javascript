ceb6d4d6395d3e9fca84630e18d3efe1d2e77daa
Kobayashi Kengo <kken91914@gmail.com>
2022-02-24 18:23:44 +0900

    enum 型の追加

---
+++ src/enum.ts
@@ -0,0 +1,20 @@
+export {};
+
+enum Month {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Month.January);
+console.log(Month.February);
+console.log(Month.December);
